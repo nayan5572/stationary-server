913e79410f5babe88fc1a9348a2cea58c84ea07f
nayan5572 <nayan.dev572@gmail.com>
2024-11-23 23:27:11 +0600

    create server

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+require('dotenv').config();
+
+const app: Application = express();
+
+// parsers
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server is Running');
+});
+
+export default app;
